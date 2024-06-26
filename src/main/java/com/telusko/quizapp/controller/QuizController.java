9d3629451016dda9c571302be0fb254f83011e8b
Pavan-kumar-coder <galipavankumar90@gmail.com>
2024-06-26 14:21:15 -0400

    question added: quiz

--- src/main/java/com/telusko/quizapp/controller/QuizController.java
+++ src/main/java/com/telusko/quizapp/controller/QuizController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 // controller for quiz
+// quiz questions added
 @RestController
 @RequestMapping("quiz")
 public class QuizController {
